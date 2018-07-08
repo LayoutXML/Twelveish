e42120cf1b87a821eaa2cda403ccb4f58a0c032b
LayoutXML <rokiux420@gmail.com>
2018-07-08 14:02:30 +0300

    Chin (flat tire) support

--- app/src/main/java/com/layoutxml/twelveish/MyWatchFace.java
+++ app/src/main/java/com/layoutxml/twelveish/MyWatchFace.java
@@ -94,6 +94,7 @@ public class MyWatchFace extends CanvasWatchFaceService {
         private boolean mRegisteredTimeZoneReceiver = false;
         private float mXOffset;
         private float mYOffset;
+        private float mChinSize;
         private Paint mBackgroundPaint;
         private Paint mTextPaint;
         private Paint mTextPaint2;
@@ -189,6 +190,7 @@ public class MyWatchFace extends CanvasWatchFaceService {
             Resources resources = MyWatchFace.this.getResources();
             isRound = insets.isRound();
             mXOffset = resources.getDimension(isRound ? R.dimen.digital_x_offset_round : R.dimen.digital_x_offset);
+            mChinSize = insets.getSystemWindowInsetBottom();
             float textSize = resources.getDimension(isRound ? R.dimen.digital_text_size_round : R.dimen.digital_text_size);
             float textSizeSmall = resources.getDimension(isRound ? R.dimen.digital_text_size_round : R.dimen.digital_text_size)/2.5f;
 
@@ -304,7 +306,7 @@ public class MyWatchFace extends CanvasWatchFaceService {
 
             //Draw date
             String text3 = String.format(Locale.UK,"%04d-%02d-%02d",mCalendar.get(Calendar.YEAR),mCalendar.get(Calendar.MONTH)+1,mCalendar.get(Calendar.DAY_OF_MONTH));
-            canvas.drawText(text3,bounds.width()/2, bounds.height()-16-mTextPaint.descent(), mTextPaint);
+            canvas.drawText(text3,bounds.width()/2, bounds.height()-16-mTextPaint.descent()-((mChinSize>0) ? mChinSize-16 : 0), mTextPaint);
         }
 
         private void updateTimer() {
