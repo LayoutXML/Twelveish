d084f6f6fe68da848d396bfeba751e63e7e44c08
LayoutXML <rokiux420@gmail.com>
2018-07-15 18:46:38 +0300

    Dummy phone settings screen
    Available through Wear OS (app by Google)

---
+++ phone/src/main/java/com/layoutxml/twelveish/MainActivity.java
@@ -0,0 +1,105 @@
+/*
+ * Copyright (c) 2018. LayoutXML
+ * Created by LayoutXML.
+ * This product is protected by copyright and distributed under licenses restricting copying, distribution and decompilation.
+ */
+
+package com.layoutxml.twelveish;
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.layoutxml.twelveish.objects.Setting;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private List<Setting> values = new ArrayList<>();
+    private SettingsAdapter mAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView mRecyclerView = findViewById(R.id.menuList);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        mAdapter = new SettingsAdapter();
+        mRecyclerView.setItemAnimator(new DefaultItemAnimator());
+        mRecyclerView.setAdapter(mAdapter);
+        generateValues();
+    }
+
+    private void generateValues() {
+        Setting setting = new Setting();
+        setting.setName("Nothing in this screen yet.\nSupport LayoutXML by purchasing \"Support Development\" app on Google Play Store. Thanks.");
+        values.add(setting);
+
+        mAdapter.notifyDataSetChanged();
+    }
+
+    public class SettingsAdapter extends RecyclerView.Adapter<SettingsAdapter.MyViewHolder>{
+
+        class MyViewHolder extends RecyclerView.ViewHolder {
+
+            TextView name;
+
+            MyViewHolder(View view) {
+                super(view);
+                Log.d(TAG,"MyViewHolder");
+                name = view.findViewById(R.id.settingsListTextView);
+
+                view.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        int position = getAdapterPosition(); // gets item position
+                        Intent intent;
+                        switch (position){
+                            /*case 0:
+                                intent = new Intent(MainActivity.this, ComplicationConfigActivity.class);
+                                MainActivity.this.startActivity(intent);
+                                break;*/
+                            default:
+                                break;
+                        }
+                    }
+                });
+            }
+        }
+
+        @NonNull
+        @Override
+        public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+            Log.d(TAG,"MyViewHolder onCreateViewHolder");
+            View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.textview_item,parent,false);
+            return new MyViewHolder(itemView);
+        }
+
+        @Override
+        public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+            Log.d(TAG,"MyViewHolder onBindViewHolder");
+            Setting setting = values.get(position);
+            holder.name.setText(setting.getName());
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return values.size();
+        }
+    }
+}
---
+++ phone/src/main/java/com/layoutxml/twelveish/objects/Setting.java
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2018. LayoutXML
+ * Created by LayoutXML.
+ * This product is protected by copyright and distributed under licenses restricting copying, distribution and decompilation.
+ */
+
+package com.layoutxml.twelveish.objects;
+
+public class Setting {
+
+    private String name;
+    private Integer icon;
+
+    public Integer getIcon() {
+        return icon;
+    }
+
+    public void setIcon(Integer icon) {
+        this.icon = icon;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
