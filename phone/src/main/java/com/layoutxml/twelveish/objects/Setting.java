f7b743f4c520e1ab3e0312d18a169e4caa6a80c6
LayoutXML <rokiux420@gmail.com>
2018-08-30 20:47:37 +0100

    Removed misleading license messages as they no longer apply

--- phone/src/main/java/com/layoutxml/twelveish/objects/Setting.java
+++ phone/src/main/java/com/layoutxml/twelveish/objects/Setting.java
@@ -1,7 +1,6 @@
 /*
  * Copyright (c) 2018. LayoutXML
  * Created by LayoutXML.
- * This product is protected by copyright and distributed under licenses restricting copying, distribution and decompilation.
  */
 
 package com.layoutxml.twelveish.objects;
