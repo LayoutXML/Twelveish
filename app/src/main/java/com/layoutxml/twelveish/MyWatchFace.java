69d78bfdc63dfac4e7e5749e5c9ed8f628c279ea
LayoutXML <hello@rokasjankunas.com>
2019-02-16 23:40:46 +0000

    Finnish line bug fix

--- app/src/main/java/com/layoutxml/twelveish/MyWatchFace.java
+++ app/src/main/java/com/layoutxml/twelveish/MyWatchFace.java
@@ -522,7 +522,7 @@ public class MyWatchFace extends CanvasWatchFaceService {
                     Suffixes = getResources().getStringArray(R.array.SuffixesFI);
                     WeekDays = getResources().getStringArray(R.array.WeekDaysFI);
                     TimeShift = new int[]{0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1};
-                    PrefixNewLine = new boolean[]{true, true, true, true, true, true, true, true, true, true, true, true};
+                    PrefixNewLine = new boolean[]{false, true, true, true, true, true, true, true, true, true, true, true};
                     SuffixNewLine = new boolean[]{false, false, false, false, false, false, false, false, false, false, false, false};
                     break;
                 case "no":
