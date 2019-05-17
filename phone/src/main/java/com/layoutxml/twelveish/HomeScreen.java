491655aef208cc70ce69f20c09967c4d5c311c45
LayoutXML <hello@rokasjankunas.com>
2019-05-17 19:05:22 +0100

    Home screen

---
+++ phone/src/main/java/com/layoutxml/twelveish/HomeScreen.java
@@ -0,0 +1,15 @@
+package com.layoutxml.twelveish;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class HomeScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.home_screen);
+    }
+
+}
