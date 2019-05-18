64020119648db4d0526953fa4f98cfdb9977cbc4
LayoutXML <hello@rokasjankunas.com>
2019-05-18 13:37:49 +0100

    customization screen 2
    tabs, pager

--- phone/src/main/java/com/layoutxml/twelveish/CustomizationScreen.java
+++ phone/src/main/java/com/layoutxml/twelveish/CustomizationScreen.java
@@ -6,8 +6,12 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager.widget.ViewPager;
 
+import com.google.android.material.tabs.TabLayout;
+import com.layoutxml.twelveish.adapters.OptionsPagerAdapter;
 import com.layoutxml.twelveish.fragments.PreviewFragment;
 
 public class CustomizationScreen extends AppCompatActivity {
@@ -22,6 +26,13 @@ public class CustomizationScreen extends AppCompatActivity {
 
         Fragment previewFragment = new PreviewFragment();
         fragmentTransaction.replace(R.id.fragmentPreview,previewFragment);
+
+        ViewPager viewPager = findViewById(R.id.pagerOptions);
+        OptionsPagerAdapter optionsPagerAdapter = new OptionsPagerAdapter(getSupportFragmentManager(), FragmentStatePagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        viewPager.setAdapter(optionsPagerAdapter);
+
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
 
         fragmentTransaction.commit();
     }
---
+++ phone/src/main/java/com/layoutxml/twelveish/adapters/OptionsPagerAdapter.java
@@ -0,0 +1,55 @@
+package com.layoutxml.twelveish.adapters;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentStatePagerAdapter;
+
+import com.layoutxml.twelveish.fragments.WatchSettingsFragment;
+
+public class OptionsPagerAdapter extends FragmentStatePagerAdapter {
+
+    public OptionsPagerAdapter(@NonNull FragmentManager fm, int behavior) {
+        super(fm, behavior);
+    }
+
+    @NonNull
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            default:
+                return new WatchSettingsFragment();
+            case 0:
+                return new WatchSettingsFragment();
+            case 1:
+                return new WatchSettingsFragment();
+            case 2:
+                return new WatchSettingsFragment();
+            case 3:
+                return new WatchSettingsFragment();
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+
+    @Nullable
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            default:
+                return null;
+            case 0:
+                return "Complications";
+            case 1:
+                return "Top";
+            case 2:
+                return "Main text";
+            case 3:
+                return "Shared";
+        }
+    }
+}
