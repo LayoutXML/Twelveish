f73901fceab257d8e94248449d4c2c31a27035c9
LayoutXML <hello@rokasjankunas.com>
2019-05-27 20:00:18 +0100

    Removed show suffixes option

--- app/src/main/java/com/layoutxml/twelveish/activities/list_activities/BooleanSwitcherActivity.java
+++ app/src/main/java/com/layoutxml/twelveish/activities/list_activities/BooleanSwitcherActivity.java
@@ -80,12 +80,6 @@ public class BooleanSwitcherActivity extends Activity {
         option.setDefaultOption(true);
         values.add(option);
 
-        option = new BooleanOption();
-        option.setName("Show Suffixes");
-        option.setKey(getString(R.string.preference_show_suffixes));
-        option.setDefaultOption(true);
-        values.add(option);
-
         mAdapter.notifyDataSetChanged();
     }
 
